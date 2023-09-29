1b98d0059a06910d3fb90a9675292dbd660ff010
SanduniSrikantha <CB010303@students.apiit.lk>
2023-09-30 00:27:57 +0530

    View Product

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ProductCard1 from './ProductCart1'
+
+const ProductCard = () => {
+  return (
+    <div class="bg-white py-10">
+        <div class="container mx-auto px-3">
+            <div class="grid md:grid-cols-1 lg:grid-cols-1 sm:grid-cols-1 gap-8">
+                <div class=" mx-auto bg-white shadow-md rounded-lg  dark:bg-gray-800 dark:border-gray-700">
+                    <ProductCard1/>
+
+                </div>
+
+            </div>
+        
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default ProductCard
