7be0f49b5de9082d68fee1f592d457ba8fb65d61
SanduniSrikantha <CB010303@students.apiit.lk>
2023-10-10 11:08:10 +0530

    mobile view nav bar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500 z-10' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-green-600 m-4'>GROWERS.</h1>
           <li className='p-4 border-b border-gray-600 uppercase'><NavLink to="/home">Products</NavLink></li>
           <li className='p-4 border-b border-gray-600 uppercase'><NavLink to="/upcoming">Upcoming</NavLink></li>
